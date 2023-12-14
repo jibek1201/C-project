8a0774e384d5e5fdf4ffe18d7b64359ae4ad2279
adelm134 <mukashevaa04@gmail.com>
2023-12-14 10:47:16 +0600

    Добавьте файлы проекта.

---
+++ Cookbook/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Cookbook
+{
+    partial class recipeManager
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
+            this.ingredientsTextBox = new System.Windows.Forms.TextBox();
+            this.recipeTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.ingredientsLabel = new System.Windows.Forms.Label();
+            this.recipeLabel = new System.Windows.Forms.Label();
+            this.recipesGridView = new System.Windows.Forms.DataGridView();
+            this.titleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.recipesGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.DarkRed;
+            this.newButton.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.newButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.newButton.Location = new System.Drawing.Point(881, 62);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(87, 35);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.DarkRed;
+            this.loadButton.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loadButton.Location = new System.Drawing.Point(881, 113);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(87, 35);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.DarkRed;
+            this.saveButton.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.saveButton.Location = new System.Drawing.Point(881, 166);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(87, 35);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.DarkRed;
+            this.deleteButton.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.deleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.deleteButton.Location = new System.Drawing.Point(881, 220);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(87, 35);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(137, 69);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(728, 22);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.Location = new System.Drawing.Point(137, 120);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(728, 22);
+            this.categoryTextBox.TabIndex = 5;
+            // 
+            // ingredientsTextBox
+            // 
+            this.ingredientsTextBox.Location = new System.Drawing.Point(137, 173);
+            this.ingredientsTextBox.Name = "ingredientsTextBox";
+            this.ingredientsTextBox.Size = new System.Drawing.Size(728, 22);
+            this.ingredientsTextBox.TabIndex = 6;
+            // 
+            // recipeTextBox
+            // 
+            this.recipeTextBox.Location = new System.Drawing.Point(137, 227);
+            this.recipeTextBox.Multiline = true;
+            this.recipeTextBox.Name = "recipeTextBox";
+            this.recipeTextBox.Size = new System.Drawing.Size(728, 22);
+            this.recipeTextBox.TabIndex = 7;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameLabel.Location = new System.Drawing.Point(14, 71);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(63, 20);
+            this.nameLabel.TabIndex = 8;
+            this.nameLabel.Text = "Name:";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryLabel.Location = new System.Drawing.Point(14, 122);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(90, 20);
+            this.categoryLabel.TabIndex = 9;
+            this.categoryLabel.Text = "Category:";
+            // 
+            // ingredientsLabel
+            // 
+            this.ingredientsLabel.AutoSize = true;
+            this.ingredientsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ingredientsLabel.Location = new System.Drawing.Point(14, 175);
+            this.ingredientsLabel.Name = "ingredientsLabel";
+            this.ingredientsLabel.Size = new System.Drawing.Size(108, 20);
+            this.ingredientsLabel.TabIndex = 10;
+            this.ingredientsLabel.Text = "Ingredients:";
+            // 
+            // recipeLabel
+            // 
+            this.recipeLabel.AutoSize = true;
+            this.recipeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.recipeLabel.Location = new System.Drawing.Point(14, 229);
+            this.recipeLabel.Name = "recipeLabel";
+            this.recipeLabel.Size = new System.Drawing.Size(73, 20);
+            this.recipeLabel.TabIndex = 11;
+            this.recipeLabel.Text = "Recipe:";
+            // 
+            // recipesGridView
+            // 
+            this.recipesGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.recipesGridView.BackgroundColor = System.Drawing.Color.IndianRed;
+            this.recipesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.recipesGridView.Location = new System.Drawing.Point(18, 311);
+            this.recipesGridView.Name = "recipesGridView";
+            this.recipesGridView.RowHeadersWidth = 51;
+            this.recipesGridView.RowTemplate.Height = 24;
+            this.recipesGridView.Size = new System.Drawing.Size(950, 167);
+            this.recipesGridView.TabIndex = 12;
+            this.recipesGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.recipesGridView_CellDoubleClick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Edwardian Script ITC", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.DarkRed;
+            this.titleLabel.Location = new System.Drawing.Point(387, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(209, 55);
+            this.titleLabel.TabIndex = 13;
+            this.titleLabel.Text = "My Recipes";
+            // 
+            // recipeManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(986, 502);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.recipesGridView);
+            this.Controls.Add(this.recipeLabel);
+            this.Controls.Add(this.ingredientsLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.recipeTextBox);
+            this.Controls.Add(this.ingredientsTextBox);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.newButton);
+            this.Name = "recipeManager";
+            this.Text = "Recipe Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.recipesGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox categoryTextBox;
+        private System.Windows.Forms.TextBox ingredientsTextBox;
+        private System.Windows.Forms.TextBox recipeTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label ingredientsLabel;
+        private System.Windows.Forms.Label recipeLabel;
+        private System.Windows.Forms.DataGridView recipesGridView;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ Cookbook/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cookbook
+{
+    public partial class recipeManager : Form
+    {
+        public object editing = false;
+        DataTable recipes = new DataTable();
+
+        public recipeManager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recipes.Columns.Add("Name");
+            recipes.Columns.Add("Category");
+            recipes.Columns.Add("Ingredients");
+            recipes.Columns.Add("Recipe");
+
+            recipesGridView.DataSource = recipes;
+        }
+
+        private void recipesGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nameTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            categoryTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            ingredientsTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[2].ToString();
+            recipeTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[3].ToString();
+            editing = true;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+            categoryTextBox.Text = "";
+            ingredientsTextBox.Text = "";
+            recipeTextBox.Text = "";
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[0].ToString();
+            categoryTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[1].ToString();
+            ingredientsTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[2].ToString();
+            recipeTextBox.Text = recipes.Rows[recipesGridView.CurrentCell.RowIndex].ItemArray[3].ToString();
+            editing = true;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if ((bool)editing)
+            {
+                recipes.Rows[recipesGridView.CurrentCell.RowIndex]["Name"] = nameTextBox.Text;
+                recipes.Rows[recipesGridView.CurrentCell.RowIndex]["Category"] = categoryTextBox.Text;
+                recipes.Rows[recipesGridView.CurrentCell.RowIndex]["Ingredients"] = ingredientsTextBox.Text;
+                recipes.Rows[recipesGridView.CurrentCell.RowIndex]["Recipe"] = recipeTextBox.Text;
+            }
+            else
+            {
+                recipes.Rows.Add(nameTextBox.Text, categoryTextBox.Text, ingredientsTextBox.Text, recipeTextBox.Text);
+            }
+            nameTextBox.Text = "";
+            categoryTextBox.Text = "";
+            ingredientsTextBox.Text = "";
+            recipeTextBox.Text = "";
+            editing = false;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                recipes.Rows[recipesGridView.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception ex) { Console.WriteLine("Not a valid row"); }
+        }
+    }
+}
